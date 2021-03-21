8ee8f6184d1603730573b9dd1716b2363fed40ab
Andrew <cotter.andrew.p@gmail.com>
2021-03-21 16:40:24 -0400

    grid start

--- mar-21/src/main.rs
+++ mar-21/src/main.rs
@@ -21,3 +21,43 @@ fn view(app: &App, _model: &Model, frame: Frame) {
 
     draw.to_frame(app, &frame).unwrap();
 }
+
+struct Grid {
+    pub cells: Vec<Cell>,
+    pub margin: f32,
+    pub row_heights: Vec<f32>,
+    pub col_widths: Vec<f32>,
+    pub palette: Vec<f32>
+}
+
+impl Grid {
+    // pub fn new() -> Grid {
+    //
+    // }
+
+    pub fn draw(){
+
+    }
+}
+
+struct Cell{
+    color: Color,
+    orientation: Direction
+}
+
+
+enum Direction {
+    Up,
+    Left,
+    Down,
+    Right,
+}
+
+fn direction_theta(dir: Direction) -> f32 {
+    match dir {
+        Direction::Up=> 0.0,
+        Direction::Left=> 90.0,
+        Direction::Down => 180.0,
+        Direction::Right => 270.0
+    }
+}
