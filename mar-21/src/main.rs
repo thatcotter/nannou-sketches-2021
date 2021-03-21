2c8add2c909c3e466f860934b22822f42395bfed
Andrew <cotter.andrew.p@gmail.com>
2021-03-21 15:18:21 -0400

    push

--- mar-21/src/main.rs
+++ mar-21/src/main.rs
@@ -1,3 +1,19 @@
+use nannou::noise::{NoiseFn, Perlin};
+use nannou::prelude::*;
+
+struct Model {}
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model).update(update).simple_window(view).run();
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+    let t = app.time;
+
+    draw.background().color(Rgb::new(0.1,0.1,0.1));
+
+    draw.to_frame(app, &frame).unwrap();
 }
